91671f7879dd0d42f2c5a9ec4f2a4014ffadaf20
matheus <matheus@sigmoidhealth.com>
2026-06-18 17:47:31 -0300

    feat(web): remove nested Recebimento/Saque tabs from finance layout

--- apps/web/app/(admin)/settings/finance/layout.tsx
+++ apps/web/app/(admin)/settings/finance/layout.tsx
@@ -1,25 +1,7 @@
-import { getTranslations } from 'next-intl/server';
-
-import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
-
 interface Props {
   children: React.ReactNode;
 }
 
-const FinanceLayout = async ({ children }: Props) => {
-  const t = await getTranslations('finance');
-  return (
-    <div>
-      <Tabs defaultValue="recebimento" className="mb-6">
-        <TabsList>
-          <TabsTrigger value="recebimento">{t('layout.tabReceiving')}</TabsTrigger>
-          <TabsTrigger value="saque" disabled>{t('layout.tabPayout')}</TabsTrigger>
-        </TabsList>
-      </Tabs>
-      {children}
-    </div>
-  );
-
-};
+const FinanceLayout = ({ children }: Props) => <>{children}</>;
 
 export default FinanceLayout;
